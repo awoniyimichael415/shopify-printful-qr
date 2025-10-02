ceb91e48490e630d9eaeebf19365c5da2813172a
awoniyimichael415 <awoniyimichael415@gmail.com>
2025-10-02 10:11:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -230,7 +230,6 @@ async function createOrUpdatePrintfulOrder(order, imageUrl) {
       email: order.email || order.customer?.email || ""
     },
     items
-    "confirm": process.env.PRINTFUL_AUTO_CONFIRM
   };
 
   console.log("🚚 Printful payload:", JSON.stringify(payload, null, 2));
